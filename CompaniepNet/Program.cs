de47fdb837305f38d783332e3db1807f591b81bd
Luis Mendez <dluismendezdj@gmail.com>
2022-05-02 11:51:29 -0400

    Enable cors and serializers

--- CompaniepNet/Program.cs
+++ CompaniepNet/Program.cs
@@ -1,11 +1,25 @@
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Enable CORS
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("http://localhost:3000/")
+        .AllowAnyMethod().AllowAnyHeader();
+}));
+
+// Json Serializer
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(
+    options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+    .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
 var app = builder.Build();
 
